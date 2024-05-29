2dc0531816158df8bb26b4061ae694465dbdd3e9
Rachael Kibicho <99732171+Rachael-Kibicho@users.noreply.github.com>
2024-05-29 04:09:19 +0300

    Update KamiLimu.js more quotes

--- KamiLimu.js
+++ KamiLimu.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const spaceSound = document.getElementById('spaceSound');
+document.addEventListener("DOMContentLoaded", () => {
+    const spaceSound = document.getElementById("spaceSound");
     spaceSound.volume = 0.5;
-    const pageTitle = document.querySelector('.container1');
-    const bodyContent = document.querySelector('.content');
+    const pageTitle = document.querySelector(".container1");
+    const bodyContent = document.querySelector(".content");
     let isSoundPlayed = false;
 
 
@@ -10,78 +10,78 @@ document.addEventListener('DOMContentLoaded', () => {
         spaceSound.play();
         isSoundPlayed = true;
     }
-    document.body.addEventListener('click', playSpaceSound, { once: true });
-    window.addEventListener('load', playSpaceSound);
+    document.body.addEventListener("click", playSpaceSound, { once: true });
+    window.addEventListener("load", playSpaceSound);
 
-    const player = videojs('myVideo', {
+    const player = videojs("myVideo", {
       controls: true,
       autoplay: false,
-      preload: 'auto',
+      preload: "auto",
     });
 
-    player.on('play', () => {
-      console.log('Video is playing!');
+    player.on("play", () => {
+      console.log("Video is playing!");
       spaceSound.pause();
-      bodyContent.classList.add('blur');
+      bodyContent.classList.add("blur");
     });
 
-    player.on('pause', () => {
-      console.log('Video is paused.');
-      bodyContent.classList.remove('blur');
+    player.on("pause", () => {
+      console.log("Video is paused.");
+      bodyContent.classList.remove("blur");
 
     });
 
-    player.on('ended', () => {
-        pageTitle.innerHTML = '<h1> Phew! You found it, I hope it was out of this world! </h1>';
+    player.on("ended", () => {
+        pageTitle.innerHTML = "<h1> Phew! You found it, I hope it was out of this world! </h1>";
         playSpaceSound();
-        bodyContent.classList.remove('blur');
+        bodyContent.classList.remove("blur");
       });
 
     const widgetImages = {
-      'widget2': 'mercuryconvo.jpg',
-      'widget3': 'venusconvo.jpg',
-      'widget4': 'earthconvo.jpg',
-      'widget5': 'marsconvo.jpg',
-      'widget6': 'jupiterconvo.jpg'
+      "widget2": "mercuryconvo.jpg",
+      "widget3": "venusconvo.jpg",
+      "widget4": "earthconvo.jpg",
+      "widget5": "marsconvo.jpg",
+      "widget6": "jupiterconvo.jpg"
     };
 
     function showModalOverlay(widgetId) {
-      const modalOverlay = document.getElementById('modalOverlay');
-      const conversation = document.getElementById('conversation');
-      const bodyContent = document.querySelector('.content');
+      const modalOverlay = document.getElementById("modalOverlay");
+      const conversation = document.getElementById("conversation");
+      const bodyContent = document.querySelector(".content");
 
       const imageUrl = widgetImages[widgetId];
       conversation.style.backgroundImage = `url(${imageUrl})`;
 
-      modalOverlay.classList.add('active');
+      modalOverlay.classList.add("active");
 
-      const myVideo = document.getElementById('myVideo');
-      const videoWidget = document.getElementById('videoWidget');
+      const myVideo = document.getElementById("myVideo");
+      const videoWidget = document.getElementById("videoWidget");
       videoWidget.addEventListener('click', () => {
-        myVideo.classList.add('active');
+        myVideo.classList.add("active");
       })
       if (widgetId !== videoWidget){
-        myVideo.classList.remove('active');
-        bodyContent.classList.remove('blur');
+        myVideo.classList.remove("active");
+        bodyContent.classList.remove("blur");
       }
     }
 
-    const widgets = document.querySelectorAll('.widget');
+    const widgets = document.querySelectorAll(".widget");
 
     widgets.forEach(widget => {
-      widget.addEventListener('click', () => {
+      widget.addEventListener("click", () => {
         showModalOverlay(widget.id);
-        bodyContent.classList.add('blur');
+        bodyContent.classList.add("blur");
       });
     });
 
-    const exitButton = document.getElementById('exit');
+    const exitButton = document.getElementById("exit");
 
-    exitButton.addEventListener('click', () => {
-      const bodyContent = document.querySelector('.content');
-      const modalOverlay = document.getElementById('modalOverlay');
-      bodyContent.classList.remove('blur');
-      modalOverlay.classList.remove('active');
+    exitButton.addEventListener("click", () => {
+      const bodyContent = document.querySelector(".content");
+      const modalOverlay = document.getElementById("modalOverlay");
+      bodyContent.classList.remove("blur");
+      modalOverlay.classList.remove("active");
       player.pause();
 
     });
